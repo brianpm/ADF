fixes to run model v model and deal with numpy sign limitation

scripts/averaging/averaging_example.py
@@ -55,8 +55,11 @@ def averaging_example(case_name, input_ts_loc, output_loc, var_list):
 
         #Average time dimension over time bands, if the bands exist:
         if 'time_bnds' in cam_ts_data:
-             cam_ts_data['time'].values = cam_ts_data['time_bnds'].mean(dim='nbnd')
-             cam_ts_data = xr.decode_cf(cam_ts_data)
+            time = cam_ts_data['time']
+            time = xr.DataArray(cam_ts_data['time_bnds'].mean(dim='nbnd').values, dims=time.dims, attrs=time.attrs)
+            cam_ts_data['time'] = time 
+            cam_ts_data.assign_coords({'time':time})
+            cam_ts_data = xr.decode_cf(cam_ts_data)
 
         #Group time series values by month, and average those months together:
         cam_climo_data = cam_ts_data.groupby('time.month').mean(dim='time')

scripts/plotting/plot_example.py
@@ -220,8 +220,8 @@ def plot_map_and_save(wks, mdlfld, obsfld, diffld, **kwargs):
     minval = np.min([np.min(mdlfld), np.min(obsfld)])
     maxval = np.max([np.max(mdlfld), np.max(obsfld)])
     normfunc, mplv = use_this_norm()
-    if np.sign(minval) != np.sign(maxval) and mplv > 2:
-        norm1 = normfunc(vmin=minval, vmax=maxval, vcenter=0.0)
+    if ((minval < 0) and (0 < maxval)) and mplv > 2:
+        norm1 = normfunc(vmin=minval, vcenter=0.0, vmax=maxval)
         cmap1 = 'coolwarm'
     else:
         norm1 = mpl.colors.Normalize(vmin=minval, vmax=maxval)