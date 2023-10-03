acc3ee07253ec2ce5fd2c76be51d7862bd2d7c4b
Brian Medeiros <brianpm@ucar.edu>
2023-10-03 17:15:09 -0600

    Only use valid netCDF4 encoding

--- scripts/regridding/regrid_and_vert_interp.py
+++ scripts/regridding/regrid_and_vert_interp.py
@@ -658,7 +658,8 @@ def save_to_nc(tosave, outname, attrs=None, proc=None):
     valid_encodings = ['_FillValue', 'fletcher32', 'complevel', 'dtype', 'zlib', 'chunksizes', 
                        'contiguous', 'compression', 'least_significant_digit', 'shuffle']
     for ec in enc:
-        enc[ec] = {key: ec[key] for key in ec if key not in valid_encodings}
+        if isinstance(enc[ec], dict):
+            enc[ec] = {key: ec[key] for key in ec if key not in valid_encodings}
 
     if attrs is not None:
         xo.attrs = attrs
