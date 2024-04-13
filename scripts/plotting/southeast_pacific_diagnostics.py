21f867e6552ec6aafc9fcc3edae9d900906d1749
Brian Medeiros <brianpm@ucar.edu>
2024-04-13 17:41:57 -0600

    Use/update adf_dataset and adapt SEPac diagnostics

--- scripts/plotting/southeast_pacific_diagnostics.py
+++ scripts/plotting/southeast_pacific_diagnostics.py
@@ -1,3 +1,4 @@
+"""Module docstring"""
 from pathlib import Path
 # import logging
 import xarray as xr
@@ -6,55 +7,42 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 
+from adf_dataset import AdfData
+
+
 
 sep_sc_box = [-20, -10, 360-90, 360-80]  # Klein & Hartmann 1993
+sep_sc_transect = [360-90, 360-70] # e.g., Bretherton et al. 2010 (10.5194/acpd-10-15921-2010)
+
+#Set seasonal ranges:
+seasons = {"ANN": np.arange(1,13,1),
+            "DJF": [12, 1, 2],
+            "JJA": [6, 7, 8],
+            "MAM": [3, 4, 5],
+            "SON": [9, 10, 11]}
+
+seasonal_cycle_var_list = ["PRECT", "SST", "CLDTOT", "SWCF", 'FSDS', 'LWCF', 'FLDS', 'FSNS', 'FSNSC', 'FLNS', 'FLNSC', 'TGCLDLWP']
+
+transect_var_list = ['CLOUD', 'CLDLIQ', 'T', 'Q']
+
 
 def southeast_pacific_diagnostics(adfobj):
-    # BOILERPLATE ADF STUFF
+    # Start by instantiating the AdfData object
+    # and Extract needed quantities from ADF object:
+    adfdata = AdfData(adfobj)
     var_list = adfobj.diag_var_list
-    model_rgrid_loc = adfobj.get_basic_info("cam_regrid_loc", required=True)
 
     #Special ADF variable which contains the output paths for
     #all generated plots and tables:
     plot_locations = adfobj.plot_location
 
-    #CAM simulation variables (this is always assumed to be a list):
-    case_names = adfobj.get_cam_info("cam_case_name", required=True)
-
     #Grab case years
     syear_cases = adfobj.climo_yrs["syears"]
     eyear_cases = adfobj.climo_yrs["eyears"]
 
-    # CAUTION:
-    # "data" here refers to either obs or a baseline simulation,
-    # Until those are both treated the same (via intake-esm or similar)
-    # we will do a simple check and switch options as needed:
-    if adfobj.get_basic_info("compare_obs"):
-        #Set obs call for observation details for plot titles
-        obs = True
-
-        #Extract variable-obs dictionary:
-        var_obs_dict = adfobj.var_obs_dict
-
-        #If dictionary is empty, then  there are no observations to regrid to,
-        #so quit here:
-        if not var_obs_dict:
-            print("\t No observations found to plot against, so no zonal-mean maps will be generated.")
-            return
-    else:
-        obs = False
-        data_name = adfobj.get_baseline_info("cam_case_name", required=True) # does not get used, is just here as a placemarker
-        data_list = [data_name] # gets used as just the name to search for climo files HAS TO BE LIST
-        data_loc  = model_rgrid_loc #Just use the re-gridded model data path
-    #End if
-
     #Grab baseline years (which may be empty strings if using Obs):
     syear_baseline = adfobj.climo_yrs["syear_baseline"]
     eyear_baseline = adfobj.climo_yrs["eyear_baseline"]
-
-    #Grab all case nickname(s)
-    test_nicknames = adfobj.case_nicknames["test_nicknames"]
-    base_nickname = adfobj.case_nicknames["base_nickname"]
 
     res = adfobj.variable_defaults # will be dict of variable-specific plot preferences
     # or an empty dictionary if use_defaults was not specified in YAML.
@@ -69,118 +57,171 @@ def southeast_pacific_diagnostics(adfobj):
     # check if existing plots need to be redone
     redo_plot = adfobj.get_basic_info('redo_plot')
     print(f"\t NOTE: redo_plot is set to {redo_plot}")
-    #-----------------------------------------
 
-    #Set data path variables:
-    #-----------------------
-    mclimo_rg_loc = Path(model_rgrid_loc)
-    if not adfobj.compare_obs:
-        dclimo_loc  = Path(data_loc)
-    #-----------------------
+    for case_idx, case_name in enumerate(adfdata.case_names):
+        #Set output plot location:
+        plot_loc = Path(plot_locations[case_idx])
 
-    #Set seasonal ranges:
-    seasons = {"ANN": np.arange(1,13,1),
-               "DJF": [12, 1, 2],
-               "JJA": [6, 7, 8],
-               "MAM": [3, 4, 5],
-               "SON": [9, 10, 11]}
-    # ---------------------
+        #Check if plot output directory exists, and if not, then create it:
+        if not plot_loc.is_dir():
+            print(f"    {plot_loc} not found, making new directory")
+            plot_loc.mkdir(parents=True)
 
-    seasonal_cycle_plot(adfobj, var_list, plot_locations, plot_type, case_names)
-    transect_plots()
-    region_climo_plots()
-
-def transect_plots():
-    pass
-
-def region_climo_plots():
-    pass
-
-def seasonal_cycle_plot(adfobj, var_list, plot_locations, plot_type, case_names):
-    ncases = len(case_names)
-    print(f"[SEPAC](seasonal_cycle_plot) N_cases = {ncases}")
-    if ncases > 1:
-        print("STOP! -- just wanted to do one case")
-        print(case_names)
-        return None
-    seasonal_cycle_var_list = ["SST", "CLDTOT", "SWCF"]
-    res = adfobj.variable_defaults
-    if adfobj.get_basic_info("compare_obs"):
-        var_obs_dict = adfobj.var_obs_dict
+        for var in var_list:
+            if var in res:
+                vres = res[var]
+            else:
+                vres = {}
+            if var in seasonal_cycle_var_list:
+                seasonal_cycle_plot(adfdata, var, vres, plot_loc, plot_type, case_name)
+            if var in transect_var_list:
+                transect_plots(adfdata, var, plot_loc, plot_type, case_name)
+            region_climo_plots()
 
 
-    for v in seasonal_cycle_var_list:
-        if v not in var_list:
-            print(f"STOP! {v} not in var_list.")
-            continue
-        if adfobj.get_basic_info("compare_obs"):
-            have_obs = v in var_obs_dict
-            print(f"LOOK IN var_obs_dict for variable: {have_obs}")
+def lonFlip(data, lonname=None):
+    # NOTE: this assumes global values
+    if lonname is None:
+        lonname = 'lon'
+    tmplon = data[lonname]
+    tmpdata = data.roll( {lonname: (len(tmplon) // 2)}, roll_coords=True)
+    lonroll = tmpdata[lonname].values
+    if tmplon.min() >= 0:
+        # flip to -180:180
+        tmpdata[lonname] = xr.DataArray(np.where(lonroll >= 180, lonroll - 360, lonroll), dims=[lonname], attrs=data[lonname].attrs)
+    else:
+        # flip from -180:180 to 0:360
+        tmpdata[lonname] = xr.DataArray(((lonroll + 360) % 360), dims=[lonname], attrs=data[lonname].attrs)
+    return tmpdata
 
-        # else
-        if v in res:
-            vres = res[v]
+
+def transect_plots(adf, var, plot_loc, plot_type, case_name):
+    """Plot 20°S transect of given variable."""
+    have_ref = var in adf.ref_var_nam
+    if not have_ref:
+        print(f"No reference data found for variable: {var}")
+    else:
+        odata = adf.load_reference_da(var)
+        if odata['lon'].min() < 0:
+            print("Flip longitude on reference data.")
+            odata = lonFlip(odata)
+        odata = odata.sel(lat=-20,method='nearest').sel(lon=slice(sep_sc_transect[0], sep_sc_transect[1]))
+    
+    mdata = adf.load_climo_da(case_name, var)
+    mdata = mdata.sel(lat=-20,method='nearest').sel(lon=slice(sep_sc_transect[0], sep_sc_transect[1]))
+
+    # Seasonal transects
+    for season, vals in seasons.items():
+        plot_name = plot_loc / f"{case_name}_{var}_{season}_sepac_20sTransect.{plot_type}"
+        print(f"[SEPAC](transect_plot) {plot_name}")
+        smdata = mdata.sel(time=vals).mean(dim='time')
+        if 'lev' in smdata.dims:
+            fig, ax = plt.subplots(ncols=2, constrained_layout=True)
+            fig.suptitle(f"20°S {var} {season}")
+            mlon, mlev = np.meshgrid(smdata.lon, smdata.lev)
+            cnModel = ax[0].pcolormesh(mlon, mlev, smdata)
+            ax[0].set_title(f"CAM")
+            ax[0].set_ylim([1000,100])
+            fig.colorbar(cnModel, ax=ax[0])
+            if have_ref:
+                sodata = odata.sel(time=vals).mean(dim='time')
+                rlon, rlev = np.meshgrid(sodata.lon, sodata.lev)
+                cnRef = ax[1].pcolormesh(rlon, rlev, sodata)
+                ax[1].set_title(adf.ref_labels[var])
+                ax[1].set_ylim([1000,100])
+                fig.colorbar(cnRef, ax=ax[1])
         else:
-            vres = {}
-        plot_loc = Path(plot_locations[0])
-
-        plot_name = plot_loc / f"{v}_sepac_seasonalcycle.{plot_type}"
-        print(f"[SEPAC](seasonal_cycle_plot) {plot_name}")
-
-
-        # load model time series files:
-        m_ts_fils = get_timeseries_file(adfobj, case_names[0], v)
-        print(f"FILES: {m_ts_fils}")
-        mdata = load_da(m_ts_fils, v, res=vres)
-        # Reduce to region:
-        mdata = mdata.sel(lat=slice(sep_sc_box[0], sep_sc_box[1]), lon=slice(sep_sc_box[2], sep_sc_box[3]))
-        # create annual cycle
-        anncyc = mdata.groupby('time.month').mean(dim='time') # (month, lat, lon)
-        avganncyc = anncyc.weighted(np.cos(np.radians(anncyc.lat))).mean(dim=("lat","lon"))
-        fig, ax = plt.subplots()
-        if 'lev' in mdata.dims:
-            print("LEV DATA -> contour")
-            mmonth, mlev = np.meshgrid(np.arange(1,13), mdata.lev)
-            cn = ax.contourf(mmonth, mlev, avganncyc)
-            fig.colorbar(cn)
-        else:
-            ax.plot(np.arange(1,13), avganncyc)
+            fig, ax = plt.subplots()
+            ax.plot(smdata.lon, smdata, label="CAM")
+            if have_ref:
+                sodata = odata.sel(time=vals).mean(dim='time')
+                ax.plot(sodata.lon, sodata, label=adf.ref_labels[var])
+            ax.legend()
         fig.savefig(plot_name, bbox_inches='tight')
 
 
-def get_timeseries_file(adfobj, case_name, field, hfile=None):
-    ts_locs = adfobj.get_cam_info("cam_ts_loc", required=True)
-    print(f"ts_locs is supposed to be: {ts_locs}")
-    if hfile is None:
-        hfile = 'h0'
-        print("Defaulting to search for `*.h0.*` files")
-    if len(ts_locs) > 1:
-        raise NotImplemented("Multi-case not supported yet")
-    ts_loc = Path(ts_locs[0])
-    ts_filenames = f'{case_name}.*.{hfile}.{field}.*nc'
-    ts_files = sorted(ts_loc.glob(ts_filenames))
-    return ts_files
 
 
-def load_dataset(fils):
-    if (len(fils) == 0):
-        print("Input file list is empty.")
-        return None
-    elif (len(fils) > 1):
-        ds = xr.open_mfdataset(fils, combine='by_coords')
+
+def region_climo_plots():
+    """Plot planar view of region."""
+    pass
+
+def seasonal_cycle_plot(adf, var, vres, plot_loc, plot_type, case_name):
+    """Plot the seasonal cycle.
+    
+    adf : adf data object
+    var : str, the name of variable to plot
+    vres : dict, the variable preferences
+    plot_location : path to directory
+    plot_type : str, extension for plots
+    case_name : str, name of case to plot
+    
+    """
+    if var not in adf.ref_var_nam:
+        print(f"No reference found for variable `{var}`.")
+
+    reference_data = adf.load_reference_dataset(var)
+    if reference_data is None:
+        print(f"No reference data found for variable: {var}")
+        have_ref = False
     else:
-        sfil = str(fils[0])
-        ds = xr.open_dataset(sfil)
-    if ds is None:
-        print(f"invalid data on load_dataset")
-    return ds
+        have_ref = True
+        odata = adf.load_reference_da(var)
+        if odata['lon'].min() < 0:
+            print("Flip longitude on reference data.")
+            odata = lonFlip(odata)
+        odata = odata.sel(lat=slice(sep_sc_box[0], sep_sc_box[1]), lon=slice(sep_sc_box[2], sep_sc_box[3]))
+        oanncyc = odata.weighted(np.cos(np.radians(odata.lat))).mean(dim=("lat","lon"))
+        print(oanncyc)
 
-def load_da(fils, variablename, res=None):
-    da = (load_dataset(fils)[variablename]).squeeze()
-    if res is None:
-        res = dict()
-    if variablename in res:
-        vres = res[variablename]
-        da = da * vres.get("scale_factor",1) + vres.get("add_offset", 0)
-        da.attrs['units'] = vres.get("new_unit", da.attrs.get('units', 'none'))
-    return da
+    plot_name = plot_loc / f"{case_name}_{var}_sepac_seasonalcycle.{plot_type}"
+    print(f"[SEPAC](seasonal_cycle_plot) {plot_name}")
+
+    # load model time series files:
+    # mdata = adf.load_regrid_da(case_name, v)
+    mdata = adf.load_climo_da(case_name, var)
+
+    # Reduce to region:
+    mdata = mdata.sel(lat=slice(sep_sc_box[0], sep_sc_box[1]), lon=slice(sep_sc_box[2], sep_sc_box[3]))
+
+    avganncyc = mdata.weighted(np.cos(np.radians(mdata.lat))).mean(dim=("lat","lon"))
+
+    if 'lev' in mdata.dims:
+        print("LEV DATA -> contour")
+        fig, ax = plt.subplots(ncols=2,nrows=2,constrained_layout=True)
+        mmonth, mlev = np.meshgrid(np.arange(1,13), mdata.lev)
+        cnModel = ax[0,0].contourf(mmonth, mlev, avganncyc)
+        ax[0,0].set_title(f"CAM")
+        rmonth, rlev = np.meshgrid(np.arange(1,13), odata.lev)
+        cnRef = ax[0,1].contourf(rmonth, rlev, oanncyc)
+        ax[0,1].set_title(f"Reference")
+        if avganncyc.shape == oanncyc.shape:
+            print("Shapes agree, will make difference panel")
+            cnDiff = ax[1,0].contourf(rmonth, rlev, avganncyc-oanncyc)
+            fig.colorbar(cnDiff, ax=ax[1,0])
+            ax[1,0].title("Model - Ref", loc='left')
+        fig.colorbar(cnModel, ax=ax[0,0])
+        fig.colorbar(cnRef, ax=ax[0,1])
+
+    else:
+        fig, ax = plt.subplots(nrows=2, sharex=True, constrained_layout=True)
+        ax[0].plot(np.arange(1,13), avganncyc, label='CAM')
+        ax[0].plot(np.arange(1,13), oanncyc, label=adf.ref_labels[var], color='gray')
+        ax[0].legend()
+        ax[0].set_title(f"{var} SEPac annual cycle", loc='left')
+        # ax[0].set_xlabel("MONTH")
+        if 'units' in mdata.attrs:
+            ax[0].set_ylabel(mdata.attrs['units'])
+        print(f"COORDS: {avganncyc.coords = }, {oanncyc.coords = }")
+        ax[1].plot(np.arange(1,13), avganncyc-oanncyc)
+        ax[1].set_title("CAM - Reference")
+        if 'units' in mdata.attrs:
+            ax[1].set_ylabel(mdata.attrs['units'])
+        ax[1].set_xlabel("MONTH")
+        [a.spines['top'].set_visible(False) for a in ax]
+        [a.spines['right'].set_visible(False) for a in ax]
+        [a.set_xlim([1,12]) for a in ax]
+
+
+    fig.savefig(plot_name, bbox_inches='tight')
